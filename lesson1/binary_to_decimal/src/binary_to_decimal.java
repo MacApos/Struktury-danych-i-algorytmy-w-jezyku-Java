87a2c99dc7c82313572707c017843f4c15973ecf
MacApos <maciejapostol98@gmail.com>
2023-06-23 13:45:48 +0200

    commit

--- lesson1/binary_to_decimal/src/binary_to_decimal.java
+++ lesson1/binary_to_decimal/src/binary_to_decimal.java
@@ -1,3 +1,17 @@
 public class binary_to_decimal {
-}
+    public static void main(String[] args) {
+        System.out.println(convert("101111"));
+    }
 
+    public static int convert(String binary){
+        int conversion = 1;
+        int result = 0;
+        for (int i = 1; i <= binary.length(); i++) {
+            if (binary.charAt(binary.length() - i) == '1') {
+                result += conversion;
+            }
+            conversion *= 2;
+        }
+        return result;
+    }
+}
